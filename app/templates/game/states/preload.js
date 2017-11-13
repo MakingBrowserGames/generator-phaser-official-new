7902eaf59a4ea5a9ccd2bb6b6cb174f2208a5326
Nguoi An Phu <mrfly911@gmail.com>
2017-11-13 10:07:37 +0000

    Update preload function

--- app/templates/game/states/preload.js
+++ app/templates/game/states/preload.js
@@ -1,30 +1,78 @@
 'use strict';
-function Preload() {
-  this.asset = null;
-  this.ready = false;
-}
+
+function Preload() {}
 
 Preload.prototype = {
-  preload: function() {
-    this.asset = this.add.sprite(this.game.width/2,this.game.height/2, 'preloader');
-    this.asset.anchor.setTo(0.5, 0.5);
 
-    this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
-    this.load.setPreloadSprite(this.asset);
-    this.load.image('yeoman', 'assets/yeoman-logo.png');
+  init: function() {
+    // still load if unfocus
+    this.stage.disableVisibilityChange = !0;
+  },
+  preload: function() {
+    // load everything here
+    this.initPreloadBar();
+    this.addLoadingText();
+    this.loadAssets();
 
   },
   create: function() {
-    this.asset.cropEnabled = false;
+    this.game.state.start("menu", !0, !1, !0);
   },
   update: function() {
-    if(!!this.ready) {
-      this.game.state.start('menu');
-    }
+
   },
-  onLoadComplete: function() {
-    this.ready = true;
+  initPreloadBar: function() {
+
+    // loadding bars
+    this.LoadingBar_Outer = this.add.image(0, 0, 'LoadingBar_Outer');
+    this.LoadingBar_Inner = this.add.sprite(0, 0, 'LoadingBar_Inner');
+
+    // Center the preload bar
+    this.LoadingBar_Outer.anchor.set(.5, .5);
+    this.LoadingBar_Outer.x = this.game.width / 2 - .5;
+    this.LoadingBar_Outer.y = this.game.height / 2;
+
+    this.LoadingBar_Inner.x = this.game.width / 2 - .5 -
+      this.LoadingBar_Inner.width / 2;
+    this.LoadingBar_Inner.y = this.game.height / 2 -
+      this.LoadingBar_Inner.height / 2 - 1.5;
+
+    this.load.setPreloadSprite(this.LoadingBar_Inner);
+  },
+  addLoadingText: function() {
+    var style = {
+      font: "45px font",
+      fill: "#FFFFFF",
+      align: "center"
+    };
+    this.loadingText = this.game.add.text(0, 0, "0%", style);
+    this.loadingText.anchor.set(.5, .5);
+    this.loadingText.position.set(this.game.world.width / 2,
+      this.game.world.height / 2 + 180);
+    this.loadingText.setShadow(2, 2, "#666666", 2);
+    this.loadingText.update();
+  },
+  loadAssets: function() {
+
+    // Fonts
+    this.load.bitmapFont("font", "assets/fonts/font.png",
+      "assets/fonts/font.fnt", null, 1);
+
+    // Imgaes
+    this.load.image('yeoman', 'assets/yeoman-logo.png');
+
+  },
+  loadUpdate: function() {
+    this.loadingText.setText(this.load.progress.toString() + "%");
+  },
+  shudown: function() {
+
+    this.LoadingBar_Outer.destroy();
+    this.LoadingBar_Inner.destroy();
+    this.loadingText.destroy();
+
   }
+
 };
 
 module.exports = Preload;
